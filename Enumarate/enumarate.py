d870ce03b92910edf65103c0eb8682eed8dc1bb0
Vijaya Sai <152338263+sankra@users.noreply.github.com>
2025-03-18 23:38:53 -0500

    Update enumarate.py

--- Enumarate/enumarate.py
+++ Enumarate/enumarate.py
@@ -2,3 +2,9 @@ fruits = ['apple', 'banana', 'cherry', 'date']
 
 for index, fruit in enumerate(fruits,start=1):
     print(f"{index}. {fruit}")
+
+#output:- 
+1. apple
+2. banana
+3. cherry
+4. date
