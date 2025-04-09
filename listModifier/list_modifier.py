f0c9522ed69357651032b8650719c2306fe13586
Vijaya Sai <152338263+sankra@users.noreply.github.com>
2025-04-09 12:44:06 -0500

    Update list_modifier.py

--- listModifier/list_modifier.py
+++ listModifier/list_modifier.py
@@ -1,9 +1,9 @@
 mylist = [2, 4, 6, 8, 10, 12, 14, 16] 
-mylist[1:5] = [] #performing indexing on the list.
+mylist[1:6] = [] #performing indexing on the list.
 print (mylist)
 
 #output
-#[2, 12, 14, 16]
+#[2,14, 16]
 
 #Here, at line no 4 we are assigning a empty list [] at mylist[1:5],thus the empty list is replacing 4,6,8,10 values in the mylist.
-#Therefore the output is [2, 12, 14, 16]
+#Therefore the output is [2,14, 16]
